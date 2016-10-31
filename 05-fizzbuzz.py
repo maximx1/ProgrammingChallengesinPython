cb97510e2688985d4359f637e6262e8429300520
Justin Walrath <walrathjaw@gmail.com>
2016-10-30 22:50:10 -0500

    Cleverer way to do fizzbuzz.

--- 05-fizzbuzz.py
+++ 05-fizzbuzz.py
@@ -4,13 +4,4 @@ print ('______________________________________')
 print ('____ THE FIZZBUZZ SEQUENCE ___________')
 print ('______________________________________')
 
-num = 0
-for num in range(100):
-	if num % 15 == 0:
-		print ('FIZZBUZZ')
-	elif num % 3 == 0:
-		print ('FIZZ')
-	elif num % 5 == 0:
-		print ('BUZZ')
-	else:
-		print (num)			
+for num in range(1, 101): print("FIZZ" * (num % 3 == 0) + "BUZZ" * (num % 5 == 0) or num)
